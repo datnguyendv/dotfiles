90757f4ba1b1271e9d4deb90ac51559b2e82633e
datnguyendv <laughing@nexondv.com>
2023-04-03 10:59:24 +0700

    update: footer email

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -37,7 +37,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "laughing@nexondv.com"
 end
 
 dashboard.section.footer.val = footer()
