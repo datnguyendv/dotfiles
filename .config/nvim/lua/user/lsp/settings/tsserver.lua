99f5706de528d5c8891a2188a3c3bcf4aa02d3f6
datnguyendv <laughing@nexondv.com>
2023-06-27 12:09:09 +0700

    update config nvim

---
+++ .config/nvim/lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,4 @@
+return { 
+  filetypes = {"javascript", "javascriptreact","javascript.js", "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
+}
