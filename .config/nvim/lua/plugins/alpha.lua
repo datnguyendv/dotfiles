126f5643e101fbb24a719e32518571aa4d9f1262
datnguyendv <nguyenvandat6901@gmail.com>
2024-06-03 10:06:17 +0700

    feat: modify alpha, tree and telescope

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -31,7 +31,7 @@ return {
       dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
       dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
       dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
-      dashboard.button("c", " " .. " Config",          "<cmd>  e $MYVIMRC <cr>"),
+      dashboard.button("c", " " .. " Config",          "<cmd> e $MYVIMRC <cr>"),
       dashboard.button("m", " " .. " Lazy Extras",     "<cmd> Mason <cr>"),
       dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
       dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
