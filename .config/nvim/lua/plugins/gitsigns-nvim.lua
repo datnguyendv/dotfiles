c93bf680b44056b7b6a4b6289d29ca2386cabfbf
laughing <laughing@nexondv.com>
2024-08-23 14:19:32 +0700

    chore: gitsign add

--- .config/nvim/lua/plugins/gitsigns-nvim.lua
+++ .config/nvim/lua/plugins/gitsigns-nvim.lua
@@ -1,7 +1,15 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  lazy = false,
-  config = function()
-    require("gitsigns").setup()
-  end
+	"lewis6991/gitsigns.nvim",
+	lazy = false,
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = true, -- Bật tính năng blame cho dòng hiện tại
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- Hiển thị blame ở cuối dòng
+				delay = 100, -- Thời gian chờ trước khi hiển thị blame
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+		})
+	end,
 }
